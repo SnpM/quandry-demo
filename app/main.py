86bf8bd8a5f47eabcf62cc9fdecf5ec37db43a19
SnpM <jpthek9@gmail.com>
2024-10-22 12:21:55 -0400

    Update package build; Add more model options; Tweak jailbreaks

--- app/main.py
+++ app/main.py
@@ -10,10 +10,39 @@ from quandry.classes import *
 from quandry.evaluators import LlmClassifier_Gemini, LlmClassifier_ChatGPT
 from quandry.subjects import OpenAiSubject
 from typing import *
+import time
 
 import mock
 import streamlit.components.v1 as components
 from streamlit_scroll_navigation import scroll_navbar
+
+
+class SubjectOption:
+    def __init__(self, name:str, subject:ISubject):
+        self.name = name
+        self.subject = subject
+subject_options = [
+    
+    SubjectOption("gpt-3.5-turbo-0125", OpenAiSubject("gpt-3.5-turbo-0125")),
+    SubjectOption("gpt-3.5-turbo-1106", OpenAiSubject("gpt-3.5-turbo-1106")),
+    SubjectOption("gpt-4", OpenAiSubject("gpt-4-0613")),
+    SubjectOption("gpt-4-turbo", OpenAiSubject("gpt-4-turbo-2024-04-09")),
+    SubjectOption("gpt-4o-mini", OpenAiSubject("gpt-4o-mini-2024-07-18")),
+    SubjectOption("gpt-4o", OpenAiSubject("gpt-4o-2024-05-13")),
+    SubjectOption("Human", mock.HumanSubject()),
+    #SubjectOption("Capital Trivia", mock.CapitalTriviaSubject())           
+] 
+
+class EvaluatorOption:
+    def __init__(self, name:str, evaluator:IEvaluator):
+        self.name = name
+        self.evaluator = evaluator
+evaluator_options = [
+    EvaluatorOption("Llm Classifier (gpt-4o)",LlmClassifier_ChatGPT()),
+    EvaluatorOption("Llm Classifier (gemini-1.5-flash)",LlmClassifier_Gemini())
+    #mock.CapitalTriviaEvaluator
+    ]
+
 
 default_prompt_df = [
     (f"{country} Capital", f"Ask for capital of {country}",
@@ -35,7 +64,6 @@ nav_labels = ["Configure Prompts", "Configure Subject", "Configure Evaluator", "
 nav_anchors = ["Configure-Prompts", "Configure-Subject", "Configure-Evaluator", "Generate-Report"]
 def main():
     st.set_page_config("Quandry Demo", layout="wide")
-
     with st.sidebar:
         st.markdown("<h1 style='text-align: center; font-size: 3em;'>Quandry</h1>", unsafe_allow_html=True)
         st.markdown("<h3 style='text-align: center;'>demo v0.1.0</h3>", unsafe_allow_html=True)
@@ -70,27 +98,14 @@ def main():
 
     #====Select Target====
     st.subheader("Configure Subject", anchor="Configure-Subject")
-    class SubjectOption:
-        def __init__(self, name:str, subject:ISubject):
-            self.name = name
-            self.subject = subject
-        
-    subject_options = [
-        SubjectOption("gpt-4o-mini", OpenAiSubject("gpt-4o-mini-2024-07-18")),   
-        SubjectOption("gpt-3.5-turbo-0125", OpenAiSubject("gpt-3.5-turbo-0125")),
-        SubjectOption("o1-mini", OpenAiSubject("o1-mini")),
-        SubjectOption("Human", mock.HumanSubject()),
-        SubjectOption("Capital Trivia", mock.CapitalTriviaSubject())           
-    ] 
 
-    if "subject_idx" not in st.session_state:
-        st.session_state["subject_idx"] = 0
+
+    st.session_state["subject_idx"] = st.session_state.get("subject_idx", 0)
     subject_idx = st.session_state["subject_idx"]
 
     subject_idx = st.selectbox(
         "Select Subject", 
-        range(len(subject_options)), 
-        index=subject_idx, 
+        range(len(subject_options)),
         format_func=lambda i:subject_options[i].name)
     st.session_state["subject_idx"] = subject_idx
     subject = subject_options[subject_idx].subject
@@ -151,7 +166,6 @@ def main():
     st.divider()
     #====Select Evaluator====
     st.subheader("Configure Evaluator", anchor="Configure-Evaluator")
-    evaluator_options = [LlmClassifier_ChatGPT, LlmClassifier_Gemini, mock.CapitalTriviaEvaluator]
 
     if "evaluator_idx" not in st.session_state:
         st.session_state["evaluator_idx"] = 0
@@ -159,12 +173,11 @@ def main():
 
     evaluator_idx = st.selectbox(
         "Select Evaluator", 
-        range(len(evaluator_options)), 
-        index=evaluator_idx, 
-        format_func=lambda i:evaluator_options[i].__name__)
+        range(len(evaluator_options)),
+        format_func=lambda i:evaluator_options[i].name)
     
     st.session_state["evaluator_idx"] = evaluator_idx
-    evaluator = evaluator_options[evaluator_idx]()
+    evaluator = evaluator_options[evaluator_idx].evaluator
 
     st.divider()
     #====Generate Report====
@@ -173,87 +186,120 @@ def main():
         
     st.subheader("Generate Report", anchor="Generate-Report")
     
-    # Set defualt current_run
-    if "current_run" not in st.session_state:
-        st.session_state["current_run"] = False
-    
-    if st.button("Run Evaluation", disabled=bool(st.session_state["current_run"])):
-        # Construct subject and evaluator with no parameters
-        # TODO: Enable passing a kwargs in; maybe define schema
-        subject_name = subject_options[subject_idx].name
-        subject = subject_options[subject_idx].subject
+    # Set default current_run
+    st.session_state["current_run"] = st.session_state.get("current_run", None)
+    st.session_state["current_run_thread"] = st.session_state.get("current_run_thread", None)
+    st.session_state["start_run"] = st.session_state.get("start_run", False)
         
-        def run(subject_name, subject):
-            tester = ExpectationTester(subject, evaluator)
-            cases = df2cases(st.session_state["cases_df"])
-            try:
-                results = tester.test_batch(cases)
-                results_df = results2df(results)
-        
-                # Format results df
-                results_df["evalcode_name"] = results_df["evalcode"].apply(lambda x: EvalCode(x).name)
-                
-                # Add to session state results
-                print ("Adding to results dict in session state")
-                # Add to beginning of reports
-                report_info = (subject_name,results_df)
-                st.session_state["reports"].insert(0, report_info)
-                st.session_state["force_tab"] = subject_name
-            except Exception as e:
-                print(f"Error running evaluation: {e}")
-                st.session_state["run_error"] = str(e)
-            finally:
-                st.session_state["current_run"] = None
-                st.session_state["current_run_thread"] = None          
-        # Check if already running
-        if not st.session_state["current_run"]:
-            st.session_state["current_run"] = subject_name
-            t = Thread(target=run, args=(subject_name, subject))
-            st.session_state["current_run_thread"] = t
-            add_script_run_ctx(t)
-            t.start()
-            st.rerun()
-    
-    status = st.empty()
-    if "current_run" in st.session_state and st.session_state["current_run"]:
-        status.text(f"Running evaluation on {st.session_state['current_run']}...")
-        t:Thread = st.session_state["current_run_thread"]
-        while t and t.is_alive():
+    def check_run() -> bool:
+        if st.session_state["start_run"]:
+            print ("Starting run")
+            st.session_state["start_run"] = False
+            subject_idx = st.session_state["subject_idx"]
+            subject_name = subject_options[subject_idx].name
+            subject = subject_options[subject_idx].subject
+            evaluator_idx = st.session_state["evaluator_idx"]
+            evaluator = evaluator_options[evaluator_idx].evaluator
+            
+            def run(subject_name, subject):
+                tester = ExpectationTester(subject, evaluator)
+                cases = df2cases(st.session_state["cases_df"])
+                try:
+                    results = tester.test_batch(cases)
+                    results_df = results2df(results)
+            
+                    # Format results df
+                    results_df["evalcode_name"] = results_df["evalcode"].apply(lambda x: EvalCode(x).name)
+                    
+                    # Add to session state results
+                    print ("Adding to results dict in session state")
+                    # Add to beginning of reports
+                    report_info = (subject_name,results_df)
+                    st.session_state["reports"].insert(0, report_info)
+                    st.session_state["force_tab"] = subject_name
+                except Exception as e:
+                    print(f"Error running evaluation: {e}")
+                    st.session_state["run_error"] = str(e)
+                finally:
+                    st.session_state["current_run"] = None
+                    st.session_state["current_run_thread"] = None   
+                    print("run thread done")
+                        
+            # Check if already running
             if st.session_state["current_run"] is None:
+                st.session_state["current_run"] = subject_name
+                t = Thread(target=run, args=(subject_name, subject))
+                st.session_state["current_run_thread"] = t
+                add_script_run_ctx(t)
+                t.start()
+            else:
+                print("Already running")
+            return True
+        else:
+            return st.session_state["current_run"] is not None
+
+    def draw_button():
+        button_container = st.empty()
+        with button_container:
+            button_disabled = st.session_state["current_run"] is not None or st.session_state["start_run"]
+            if st.button("Run Evaluation", disabled=button_disabled, key="run_button"):
+                st.session_state["start_run"] = True    
+                print("click")
                 st.rerun()
-        st.session_state["current_run"] = None
-        st.session_state["current_run_thread"] = None
-        st.rerun()
-    if "run_error" in st.session_state:
-        status.markdown(f"<span style='color:red;'>Error running evaluation: {st.session_state['run_error']}</span>", unsafe_allow_html=True)
-        del st.session_state["run_error"]
-        
-    # Display all reports in one tab per report  
-    reports = st.session_state["reports"]
-    tab_labels = [x[0] for x in reports]
+                    
+    status_container = st.empty()
     
-    if len(tab_labels) > 0:
-        force_tab = st.session_state.get("force_tab", None)
-        if force_tab is not None:
-            st.session_state["force_tab"] = None
-            # Little hack to reset tabs to select first tab
-            components.html("<script>frameElement.parentElement.style.display = 'none';</script>",)
+    reports_container = st.empty()
+    def draw_report(crib=""):
+        with reports_container:
+            # Display all reports in one tab per report  
+            reports = st.session_state["reports"]
+            tab_labels = [x[0] for x in reports]
             
-        tabs = st.tabs(tab_labels)
-        for report_info, tab in zip(reports, tabs):
-            with tab:
-                result_df = report_info[1]
-                column_config={
-                    "explanation":st.column_config.Column(width="large"),
-                    "evalcode_name":st.column_config.Column(width="small")
-                }
-                st.dataframe(
-                    result_df[['prompt', 'response', 'evalcode_name', 'explanation']], 
-                    use_container_width=True, 
-                    hide_index=True,
-                    column_config=column_config
-                )
-            
+            if len(tab_labels) > 0:
+                force_tab = st.session_state.get("force_tab", None)
+                if force_tab is not None:
+                    st.session_state["force_tab"] = None
+                    # Little hack to reset selection to first tab
+                    st.write("")
+                    st.rerun()  
+                tabs = st.tabs(tab_labels)
+                i = 0
+                for report_info, tab in zip(reports, tabs):
+                    with tab:
+                        result_df = report_info[1]
+                        column_config={
+                            "explanation":st.column_config.Column(width="large"),
+                            "evalcode_name":st.column_config.Column(width="small")
+                        }
+                        st.dataframe(
+                            result_df[['prompt', 'response', 'evalcode_name', 'explanation']], 
+                            use_container_width=True, 
+                            hide_index=True,
+                            column_config=column_config,
+                            key = i)
+                        i += 1
+    draw_button()
+    running = check_run()
+    
+    if running:
+        draw_report()
+        current_run = st.session_state["current_run"]
+        t = st.session_state["current_run_thread"]
+        with status_container:
+            with st.spinner(f"Running evaluation on {current_run}..."): 
+                while t and t.is_alive():
+                    time.sleep(.25)
+                print("spin done for ", t)
+        st.rerun()
+    else:
+        draw_report()
+        if "run_error" in st.session_state:
+            with status_container:
+                st.markdown(f"<span style='color:red;'>Error running evaluation: {st.session_state['run_error']}</span>", unsafe_allow_html=True)
+            del st.session_state["run_error"]
+        else:
+            status_container.text("")
     
 if __name__ == "__main__":
     main()
