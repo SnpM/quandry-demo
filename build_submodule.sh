393adf638ca884a51a64275fb00df0a84aa22c1b
SnpM <jpthek9@gmail.com>
2024-10-23 16:45:12 -0400

    Update quandry; Add quandry build script

---
+++ build_submodule.sh
@@ -0,0 +1,8 @@
+# build python module from ./quandry_package
+python -m build quandry_package
+
+# copy .whl file to .
+cp ./quandry_package/dist/quandry*.whl .
+
+# install the module
+pip install quandry*.whl --force
